a776f6840e46295d6d45681548bcd56db4059bcd
djcadaniel <djcadaniel@gmail.com>
2024-07-22 11:42:43 -0500

    add appPropinas

---
+++ src/reducers/order-reducer.ts
@@ -0,0 +1,57 @@
+import { MenuItem, OrderItem } from "../types";
+
+export type OrderActions = 
+  { type: 'add-item', payload: { item: MenuItem } } |
+  { type: 'remove-item', payload: {id: MenuItem['id']} } |
+  { type: 'place-order'} |
+  { type: 'add-tip', payload: {value: number} }
+
+export type OrderState = {
+  order: OrderItem[],
+  tip: number
+}
+
+export const initialState: OrderState = {
+  order:[],
+  tip: 0
+}
+
+export const orderReducer = (
+  state: OrderState = initialState,
+  action: OrderActions
+) => {
+
+  if(action.type === 'add-item'){
+
+    const itemExist = state.order.find(orderItem => orderItem.id === action.payload.item.id);
+    let updatedOrder:OrderItem[] = [];
+        if(itemExist) {
+          updatedOrder = state.order.map( orderItem => orderItem.id === action.payload.item.id ? 
+                {...orderItem, quantity: orderItem.quantity + 1 } 
+              : 
+                orderItem
+            )
+        } else {
+            const newItem  = {...action.payload.item, quantity: 1}
+            updatedOrder = [...state.order, newItem]
+        }
+
+    return{
+      ...state,
+      order: updatedOrder
+    }
+  }
+  if(action.type === 'place-order'){
+    return{
+      ...state
+    }
+  }
+  if(action.type === 'remove-item'){
+    return{
+      ...state
+    }
+  }
+
+  return state
+
+}
